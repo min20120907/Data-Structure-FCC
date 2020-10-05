de5a484db2c4faad093b9b8b3412123f1b149eb4
CangyuLin <jefflin.je598@gmail.ocm>
2020-10-05 12:50:10 +0800

    add fractions reader

--- frac_binread.c
+++ frac_binread.c
@@ -1,13 +1,32 @@
 #include<stdio.h>
 
+typedef struct Frac{
+	int numerator;
+	int denominator;
+}frac;
+
+int cmpfrac(const void *a, const void *b){
+	return ((*(frac*)a).numerator / (*(frac*)a).denominator) - ((*(frac*)b).numerator / (*(frac*)b).denominator);
+}
+
+void printFrac (frac a){
+	printf("%d/%d \n", a.numerator, a.denominator);
+}
+
 int main(int argc, char **argv){
-	unsigned char buffer[110];
 	FILE *ptr;
 
 	ptr = fopen("fraction_binary.bin","rb");  // r for read, b for binary
+	frac frac_arr[10];
+	//read binary file's fractions
+	int i;
+	for ( i = 0; i  < 10; i++ ){
+		fscanf(ptr, "%d / %d",&frac_arr[i].numerator, &frac_arr[i].denominator);
+	}
+	printf("All the fractions: \n");
+	for(i=0;i<10;i++)
+		printFrac(frac_arr[i]);
 
-	fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
-
-	printf("%s", buffer);
+	//qsort();
 	return 0;
 }
