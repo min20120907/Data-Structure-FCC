3c23a49f5781a62773536937016baeb4d272d854
CangyuLin <jefflin.je598@gmail.ocm>
2020-11-09 14:13:08 +0800

    fix everything contains bugs.

--- infix-postfix/infix-3.c
+++ infix-postfix/infix-3.c
@@ -1,102 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int priority(char op)
-{
-    switch (op)
-    {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-    case '%':
-        return 2;
-    default:
-        return 0;
-    }
+#include<stdio.h>
+#include<stdlib.h>
+void removeSubstr (char *string, char *sub) {
+	char *match;
+	int len = strlen(sub);
+	while ((match = strstr(string, sub))) {
+		*match = '\0';
+		strcat(string, match+len);
+	}
 }
-int is_operator(char c)
-{
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '%';
+int priority(char op) { 
+    switch(op) { 
+        case '+': case '-': return 1;
+        case '*': case '/': case '%': return 2;
+        default:            return 0;
+    } 
+}
+int is_operator(char c){
+        return c=='+' || c=='-' || c=='*' || c=='/' || c=='(' || c==')' || c=='%' ;
 }
 
-char *to_postfix(const char *infix)
-{
+char *to_postfix(const char *infix) {
+    
+    char *postfix = malloc(sizeof(char)*101);
+    char *stack = malloc(sizeof(char)*50);
 
-    char *postfix = malloc(sizeof(char) * 101);
-    char *stack = malloc(sizeof(char) * 50);
+    int i, j, top;
 
-    int i, j, top, k = 1;
+    for(i = 0, j = 0, top = 0; infix[i] != '\0'; i++){
+        
+   switch(infix[i]) { 
+       
+        case '(': 
+            stack[++top] = infix[i]; 
 
-    for (i = 0, j = 0, top = 0; infix[i] != '\0'; i++)
-    {
-
-        switch (infix[i])
-        {
-        case '(':
-            stack[++top] = infix[i];
-            break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            while (priority(stack[top]) >= priority(infix[i]))
-            {
+            break; 
+        case '+': case '-': case '*': case '/': 
+        while(priority(stack[top]) >= priority(infix[i])) {
                 postfix[j++] = stack[top--];
-                postfix[j++] = ' ';
-            }
-            if (stack[top + 1] != ' ' || stack[top] != ' ')
-                stack[++top] = ' ';
-            stack[++top] = infix[i];
-            break;
-        case ')':
-            while (stack[top] != '(')
-            {
+                //postfix[j++] = ' ';
+            } 
+            postfix[j++] = ' ';
+            
+            stack[++top] = infix[i];  
+            
+            break; 
+        case ')': 
+            
+           while(stack[top] != '(') { 
                 postfix[j++] = stack[top--];
-            }
-            top--;
-            break;
-        case ' ':
-            break;
+                    
+            } 
+            top--;  
+             break; 
+        case ' ': 
+        break;
+        case '.':
+        postfix[j++] = infix[i];
+        break;
         default:
             postfix[j++] = infix[i];
-            while (infix[i + k] == ' ')
-                k++;
-            if (is_operator(infix[i + k]))
-            {
-                postfix[j++] = ' ';
-                k = 1;
-            }
-        }
+    }   
+    
     }
-    if (stack[top + 2] != ' ')
-        stack[++top] = ' ';
-    while (top > 0)
-    {
-        //printf("stack: %c\n", stack[top--]);
-        //top++;
+
+
+    while(top > 0) { 
+        
+        postfix[j++] = ' ';
         postfix[j++] = stack[top--];
+        
     }
-    int l = 0;
-    int spaced = 0;
-    int spaces = 0;
-    while (l < j)
-    {
-        if ((postfix[l] == ' ' && postfix[l + 1] == ' ') || spaced)
-        {
-            postfix[l] = postfix[l - 1];
-            spaces++;
-            spaced = 1;
-            //printf("too much white spaces");
+    int m = 0;
+    int wrong_spaces = 0;
+    int *space_indice = malloc(sizeof(int)*100);
+    int n = 0;
+    for(;m<j;m++){
+        if(is_operator(postfix[m+1])&&postfix[m] != ' '){
+            wrong_spaces++;
+            space_indice[n++] = m+1;
+            //printf("space indice: %d \n", m+1);
+            char *tmp=malloc(sizeof(char)*100);
+            memcpy(tmp, postfix, m+1 );
+            
+            char *tmp2=malloc(sizeof(char)*100);
+            memcpy(tmp2, postfix, strlen(postfix)+1);
+            removeSubstr(tmp2,tmp);
+            //tmp2 = strcat(" ", tmp2);
+            //printf("tmp,tmp2:%s %s\n",tmp,tmp2);
+            sprintf(postfix, "%s %s", tmp, tmp2);
+            
         }
-        l++;
     }
-    //printf("%d", spaces);
-    int m = 1;
-    for (; m <= spaces; m++)
-        postfix[l - m] = 0;
-    return postfix;
+
+    //printf("wrong spaces: %d\n", wrong_spaces);
+    //printf("new string: %s\n", str);
+
+return postfix;
 }
 int main(int argc, char **argv)
 {
