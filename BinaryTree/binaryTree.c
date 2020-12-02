0e7823d16adb02285c49a0d7a22b11d9486c11e9
Cangyu <jefflin.je598@gmail.com>
2020-12-02 08:14:18 +0800

    create a delete root functin

--- BinaryTree/binaryTree.c
+++ BinaryTree/binaryTree.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _TreeNode {
+typedef struct _TreeNode
+{
 	double key;
 	struct _TreeNode *left, *right;
-}TreeNode, *TreeNodePtr;
+} TreeNode, *TreeNodePtr;
 
-TreeNodePtr deleteRoot (TreeNodePtr T);
+TreeNodePtr deleteRoot(TreeNodePtr T, double key){
+	TreeNodePtr tmp;
+	if (isInBST(T, key)) return T;
+	else {
+		
+	}
+	return tmp;
+}
 
 // Allow a null pointer here.
 // Case 1: When root node is null.
 // Case 2: When the root node has value, same as key, do nothing, return itself.
 // Case 3: When the root node has value, larger than key, add to the left, do the recursive call.
 // Case 4: When the root node has value, less than key, add to the right, do the recursive call.
-TreeNodePtr addToBST(TreeNodePtr root, double key) {
-	if(root == NULL) {	// Case 1
-		TreeNodePtr n = (TreeNodePtr) malloc(sizeof(TreeNode));
+TreeNodePtr addToBST(TreeNodePtr root, double key)
+{
+	if (root == NULL)
+	{ // Case 1
+		TreeNodePtr n = (TreeNodePtr)malloc(sizeof(TreeNode));
 		n->key = key;
-		n->left = NULL; n->right = NULL;
+		n->left = NULL;
+		n->right = NULL;
 		return n;
 	}
-	if(root->key == key){	// Case 2
+	if (root->key == key)
+	{ // Case 2
 		return root;
 	}
-	if(key < root->key) {	// Case 3
+	if (key < root->key)
+	{ // Case 3
 		root->left = addToBST(root->left, key);
 		return root;
-	} else {	// Case 4
+	}
+	else
+	{ // Case 4
 		root->right = addToBST(root->right, key);
 		return root;
 	}
@@ -38,16 +53,26 @@ TreeNodePtr addToBST(TreeNodePtr root, double key) {
 // Case 3: Whether it is in the left tree, do the recursive call, and return true.
 // Case 4: Whether it is in the right tree, do the recursive call, and return true.
 // Case 5: If the given key is different, then return false.
-int isInBST(TreeNodePtr root, double key) {
-	if(root == NULL) {	// Case 1
+int isInBST(TreeNodePtr root, double key)
+{
+	if (root == NULL)
+	{ // Case 1
 		return 0;
-	} else if (root->key == key) {	// Case 2
+	}
+	else if (root->key == key)
+	{ // Case 2
 		return 1;
-	} else if(isInBST(root->left, key)) {	// Case 3
+	}
+	else if (isInBST(root->left, key))
+	{ // Case 3
 		return 1;
-	} else if(isInBST(root->right, key)) {	// Case 4
+	}
+	else if (isInBST(root->right, key))
+	{ // Case 4
 		return 1;
-	} else {	// Case 5
+	}
+	else
+	{ // Case 5
 		return 0;
 	}
 
@@ -57,28 +82,33 @@ int isInBST(TreeNodePtr root, double key) {
 	// && (root->key == key)
 	// || isInBST(root->left, key)
 	// || isInBST(root->right, key);
-
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 	int i;
 	double k;
 	TreeNodePtr root = NULL;
-	if(argc > 1) {
-		
-		for (i = 1; i<argc; i++) {
+	if (argc > 1)
+	{
+
+		for (i = 1; i < argc; i++)
+		{
 			double d;
 			sscanf(argv[i], "%lf", &d);
 			root = addToBST(root, d);
 		}
 	}
-	
+
 	printf("Test value: ");
 	scanf("%lf", &k);
-	
-	if(isInBST(root, k)) {
+
+	if (isInBST(root, k))
+	{
 		printf("Found \n");
-	} else {
+	}
+	else
+	{
 		printf("Not Found \n");
 	}
 	return 0;
