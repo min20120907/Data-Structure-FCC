bc3bd35886901388146fba14bac51f815f2b7e1b
Cangyu Lin <jefflin.je598@gmail.com>
2021-01-07 14:43:18 +0800

    add directed graph header files

---
+++ graph/directed_graph.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	ver_p g[7];
+	return 0;
+}
---
+++ graph/directed_graph.h
@@ -0,0 +1,13 @@
+#ifndef directed_graph_h
+#define directed_graph_h
+typedef struct _Vinfo{
+	char name[100];
+	struct _Ainfo *adj;
+}ver, *ver_p;
+
+typedef struct _AInfo{
+	int V_idx;
+	double weight;
+	struct _AInfo *next;
+}adj, adj_p;
+#endif
